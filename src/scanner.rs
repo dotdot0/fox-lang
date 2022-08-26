fa61c0046f1387a81850b8d9ee72f868b2f69c92
“pratushrai0309” <pratushrai0309@gmail.com>
2022-08-27 01:06:19 +0530

    Scanner

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,4 +1,5 @@
-use crate::{token::*, token_type::TokenType};
+#![allow(dead_code)]
+use crate::{token::{*, self}, token_type::TokenType};
 
 pub struct Scanner{
     pub source:  String,
@@ -33,7 +34,7 @@ impl Scanner{
         self.current >= self.source.len()
     }
 
-    fn scan_token(&self){
+    fn scan_token(&mut self){
         let c = self.advance();
         match c{
             '(' => self.add_token(TokenType::LeftParen),
@@ -45,15 +46,72 @@ impl Scanner{
             '-' => self.add_token(TokenType::Minus),
             '+' => self.add_token(TokenType::Plus),
             ';' => self.add_token(TokenType::Semicolon),
-            '*' => self.add_token(TokenType::Star)
+            '*' => self.add_token(TokenType::Star),
+            '!' => {
+                let tok = if self.is_match('='){
+                    TokenType::Bang_Equal
+                }else{
+                    TokenType::Bang
+                };
+                self.add_token(tok)
+            },
+            '=' => {
+                let tok = if self.is_match('='){
+                    TokenType::Equal_Equal
+                }else{
+                    TokenType::Equal
+                };
+                self.add_token(tok);
+            },
+            '<' => {
+                let tok = if self.is_match('='){
+                    TokenType::Less_Equal
+                }else{
+                    TokenType::Less
+                };
+                self.add_token(tok)
+            }
+            '>' => {
+                let tok = if self.is_match('='){
+                    TokenType::Greater_Equal
+                }else{
+                    TokenType::Greater
+                };
+                self.add_token(tok);
+            },
+            _ => {
+                unreachable!("unexpected character")
+            }
+    }
+    }
+
+    fn advance(&mut self) -> char{
+        let result = self.source.chars().nth(self.current).unwrap();
+        self.current = self.current + 1;
+        result
+    }
+
+    fn add_token(&mut self,ttype: TokenType) {
+        self.add_token_object(ttype, None)
+    }
+
+    fn add_token_object(&mut self, ttype: TokenType, literal: Option<Object>){
+        let lexeme: String = self.source[self.start..self.current].to_string();
+        self.tokens.push(Token::new(ttype, lexeme, literal, self.line))
+    }
+
+    fn is_match(&mut self,expected: char) -> bool{
+        if self.is_at_end(){
+            false
+        }
+        else if self.source.chars().nth(self.current).unwrap() != expected{
+            false
+        }
+        else{
+            self.current +=  1;
+            true
         }
     }
 
-    fn advance(&self) -> char{
-        self.source.chars().nth(self.current + 1).unwrap()
-    }
-
-    fn add_token(&self,token: TokenType) {
-
-    }
+    
 }  
--- src/token.rs
+++ src/token.rs
@@ -1,19 +1,33 @@
+
+use std::usize;
+
 use crate::token_type::*;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Object{
-
+    Num(f32),
+    str(String),
+    Bool(bool),
+    Nil
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Token{
-    pub ttype: TokenType,
-    pub lexeme: String,
-    pub literal: Option<Object>,
-    pub line: usize
+     ttype: TokenType,
+     lexeme: String,
+     literal: Option<Object>,
+     line: usize
 }
 
 impl Token {
+    pub fn new(ttype: TokenType, lexeme: String, literal: Option<Object>, line:usize) -> Token{
+        Token {
+            ttype,
+            lexeme,
+            literal,
+            line
+        }
+    }
     pub fn eof(line:usize) -> Token{
         Token { ttype: TokenType::EOF, lexeme: "".to_owned(), literal: None, line }
     }
