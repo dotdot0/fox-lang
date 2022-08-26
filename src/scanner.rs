8637c6f2522310c9d75b0382aed95f2b34e03e36
“pratushrai0309” <pratushrai0309@gmail.com>
2022-08-26 20:34:23 +0530

    Token

--- src/scanner.rs
+++ src/scanner.rs
@@ -2,18 +2,24 @@ use crate::token::*;
 
 pub struct Scanner{
     pub source:  String,
-    pub tokens: Vec<Token>
+    pub tokens: Vec<Token>,
+    start: usize,
+    current: usize, 
+    line: usize
 }
 
 impl Scanner{
     pub fn new(source: String) -> Scanner{
         Scanner{
             source,
-            tokens: Vec::new()
+            tokens: Vec::new(),
+            current: 0,
+            start: 0,
+            line: 1
         }
     }
 
-    pub fn scan_token(){
+    pub fn scan_token() -> Result<>{
 
     }
 }  
